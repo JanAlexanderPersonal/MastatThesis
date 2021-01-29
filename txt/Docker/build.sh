78a94809c9022ff1110c0dd993148fadd4561532
jalexander <jan.alexander@verhaert.com>
2021-01-29 17:11:01 +0100

    deletion of txt folder

--- txt/Docker/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Build a new image for reiss
-echo "clean up docker"
-docker system prune -a
-echo "Start to build thesis:pdflatex"
-echo "---"
-docker build --pull --rm -f "Dockerfile" -t jal_pdflatex:pdflatex "." 
-echo "--"
-echo "Done"
