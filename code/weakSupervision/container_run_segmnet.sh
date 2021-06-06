b41caadca974d24694fef050bb0786af84e3e4a4
Jan Alexander <jan.alexander@verhaert.com>
2021-06-06 20:51:56 +0200

    Remove double container run segment

--- code/weakSupervision/container_run_segmnet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Run the torch container
-docker run --rm --gpus all --net=host --ipc=host -m 24g -e DISPLAY=$DISPLAY \
-    -v ${PWD}:/root/space/code \
-    -v /media/jan/DataStorage/ProjectData/spine_volumes/:/root/space/data/ \
-    -v /media/jan/DataStorage/ProjectData/temp/:/root/space/temp/ \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    --env="QT_X11_NO_MITSHM=1" -it jal:torch_thesis bash
