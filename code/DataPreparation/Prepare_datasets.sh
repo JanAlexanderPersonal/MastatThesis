c45105bb50a6093b5f669adb57802b32d2da0ff9
jalexander <jan.alexander@verhaert.com>
2021-06-13 14:04:40 +0200

    Data preparation with different contrast enhancements

--- code/DataPreparation/Prepare_datasets.sh
+++ code/DataPreparation/Prepare_datasets.sh
@@ -2,160 +2,35 @@
 #!/bin/bash
 # run the data preprocessor
 
-
-
-python3.8 /root/space/code/DataPreparation/prepare_USiegen.py \
-        --source /root/space/data/USiegen/ \
-        --output /root/space/temp/dataset_2/ \
-        --dimension 2
-
-
-
-python3.8 /root/space/code/DataPreparation/prepare_MyoSegmenTUM.py \
-        --source /root/space/data/OSF_Sarah_Schlaeger/ \
-        --output /root/space/temp/dataset_2/ \
-        --dimension 2
-
-
-
-python3.8 /root/space/code/DataPreparation/prepare_xVertSeg.py \
-        --source /root/space/data/xVertSeg/Data1/ \
-        --output /root/space/temp/dataset_2/ \
-        --dimension 2
-
-python3.8 /root/space/code/DataPreparation/prepare_USiegen.py \
-        --source /root/space/data/USiegen/ \
-        --output /root/space/temp/dataset_1/ \
-        --dimension 1
-
-
-
-python3.8 /root/space/code/DataPreparation/prepare_MyoSegmenTUM.py \
-        --source /root/space/data/OSF_Sarah_Schlaeger/ \
-        --output /root/space/temp/dataset_1/ \
-        --dimension 1
-
-
-
-python3.8 /root/space/code/DataPreparation/prepare_xVertSeg.py \
-        --source /root/space/data/xVertSeg/Data1/ \
-        --output /root/space/temp/dataset_1/ \
-        --dimension 1
-
-python3.8 /root/space/code/DataPreparation/prepare_USiegen.py \
-        --source /root/space/data/USiegen/ \
-        --output /root/space/temp/dataset_0/ \
-        --dimension 0
-
-
-
-python3.8 /root/space/code/DataPreparation/prepare_MyoSegmenTUM.py \
-        --source /root/space/data/OSF_Sarah_Schlaeger/ \
-        --output /root/space/temp/dataset_0/ \
-        --dimension 0
-
-
-
-python3.8 /root/space/code/DataPreparation/prepare_xVertSeg.py \
-        --source /root/space/data/xVertSeg/Data1/ \
-        --output /root/space/temp/dataset_0/ \
-        --dimension 0
-
 # Contrast enhancement preprocessing
 
+for c in 0 1 3
 
+do
+        for d in 0 1 2
+        do
 
-python3.8 /root/space/code/DataPreparation/prepare_USiegen.py \
-        --source /root/space/data/USiegen/ \
-        --output /root/space/temp/dataset_2_contrast_1/ \
-        --contrast 1 \
-        --dimension 2
-
-
-
-python3.8 /root/space/code/DataPreparation/prepare_MyoSegmenTUM.py \
-        --source /root/space/data/OSF_Sarah_Schlaeger/ \
-        --output /root/space/temp/dataset_2_contrast_1/ \
-        --contrast 1 \
-        --dimension 2
+                python3.8 /root/space/code/DataPreparation/prepare_USiegen.py \
+                        --source /root/space/data/USiegen/ \
+                        --output /root/space/temp/dataset_${d}_contrast_$c/ \
+                        --contrast $c \
+                        --dimension $d
 
 
 
-python3.8 /root/space/code/DataPreparation/prepare_xVertSeg.py \
-        --source /root/space/data/xVertSeg/Data1/ \
-        --output /root/space/temp/dataset_2_contrast_1/ \
-        --contrast 1 \
-        --dimension 2
-
-
-## Contrast 2
-
-python3.8 /root/space/code/DataPreparation/prepare_USiegen.py \
-        --source /root/space/data/USiegen/ \
-        --output /root/space/temp/dataset_2_contrast_2/ \
-        --contrast 2 \
-        --dimension 2
+                python3.8 /root/space/code/DataPreparation/prepare_MyoSegmenTUM.py \
+                        --source /root/space/data/OSF_Sarah_Schlaeger/ \
+                        --output /root/space/temp/dataset_${d}_contrast_$c/ \
+                        --contrast $c \
+                        --dimension $d
 
 
 
-python3.8 /root/space/code/DataPreparation/prepare_MyoSegmenTUM.py \
-        --source /root/space/data/OSF_Sarah_Schlaeger/ \
-        --output /root/space/temp/dataset_2_contrast_2/ \
-        --contrast 2 \
-        --dimension 2
+                python3.8 /root/space/code/DataPreparation/prepare_xVertSeg.py \
+                        --source /root/space/data/xVertSeg/Data1/ \
+                        --output /root/space/temp/dataset_${d}_contrast_$c/ \
+                        --contrast $c \
+                        --dimension $d
 
-
-
-python3.8 /root/space/code/DataPreparation/prepare_xVertSeg.py \
-        --source /root/space/data/xVertSeg/Data1/ \
-        --output /root/space/temp/dataset_2_contrast_2/ \
-        --contrast 2 \
-        --dimension 2
-
-## Contrast 3
-
-python3.8 /root/space/code/DataPreparation/prepare_USiegen.py \
-        --source /root/space/data/USiegen/ \
-        --output /root/space/temp/dataset_2_contrast_3/ \
-        --contrast 3 \
-        --dimension 2
-
-
-
-python3.8 /root/space/code/DataPreparation/prepare_MyoSegmenTUM.py \
-        --source /root/space/data/OSF_Sarah_Schlaeger/ \
-        --output /root/space/temp/dataset_2_contrast_3/ \
-        --contrast 3 \
-        --dimension 2
-
-
-
-python3.8 /root/space/code/DataPreparation/prepare_xVertSeg.py \
-        --source /root/space/data/xVertSeg/Data1/ \
-        --output /root/space/temp/dataset_2_contrast_3/ \
-        --contrast 3 \
-        --dimension 2
-
-## Contrast 4
-
-python3.8 /root/space/code/DataPreparation/prepare_USiegen.py \
-        --source /root/space/data/USiegen/ \
-        --output /root/space/temp/dataset_2_contrast_4/ \
-        --contrast 4 \
-        --dimension 2
-
-
-
-python3.8 /root/space/code/DataPreparation/prepare_MyoSegmenTUM.py \
-        --source /root/space/data/OSF_Sarah_Schlaeger/ \
-        --output /root/space/temp/dataset_2_contrast_4/ \
-        --contrast 4 \
-        --dimension 2
-
-
-
-python3.8 /root/space/code/DataPreparation/prepare_xVertSeg.py \
-        --source /root/space/data/xVertSeg/Data1/ \
-        --output /root/space/temp/dataset_2_contrast_4/ \
-        --contrast 4 \
-        --dimension 2
+        done
+done
