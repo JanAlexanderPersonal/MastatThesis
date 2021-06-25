2c87436dcbfcfd9168aded1997ddb9224c710668
jalexander <jan.alexander@verhaert.com>
2021-06-25 11:57:27 +0200

    Network visualization

--- text/run_latex_docker.sh
+++ text/run_latex_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # run the latest pdf container
 
-python PlotNeuralNets/unet.py 
+# python PlotNeuralNets/unet.py 
 python PlotNeuralNets/vgg16_upscore.py 
 
 docker run --rm -it -v ${PWD}:/home/thesis/ jal:pdflatex
