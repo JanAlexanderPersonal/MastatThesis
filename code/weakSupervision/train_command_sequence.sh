fbc8eb8182a06f5e0bcff1a94493a97af1c24481
Jan Alexander <jan.alexander@verhaert.com>
2021-07-04 15:11:02 +0200

    adapt train command sequence script

--- code/weakSupervision/train_command_sequence.sh
+++ code/weakSupervision/train_command_sequence.sh
@@ -15,18 +15,9 @@ do
     done
 done
 
-for c in 0 1
-do
-    for d in 2 1 0
-    do
-        
-        python3.8 trainval.py -e full_spine_dataset_c6 -sb $PATH_OP/results_full_dataset_${d}_contrast_$c -d $PATH_OP/dataset_${d}_contrast_$c/ -tb $PATH_OP/tensorboard_full_d_${d}_c_${c} -r 0
 
-        
-    done
-done
 
-for c in 3 0 1
+for c in 3 
 
 do
     for d in 2 1 0
@@ -39,3 +30,25 @@ do
 done
 
 
+for c in 0 1
+do
+    for d in 2 
+    do
+        
+        python3.8 trainval.py -e full_spine_dataset_c6_red -sb $PATH_OP/results_full_dataset_${d}_contrast_$c -d $PATH_OP/dataset_${d}_contrast_$c/ -tb $PATH_OP/tensorboard_full_d_${d}_c_${c} -r 0
+
+        
+    done
+done
+
+for c in 0 1
+
+do
+    for d in 2 1 0
+    do
+        
+        python3.8 trainval.py -e weakly_spine_dataset_c6_weighted_red -sb $PATH_OP/results_weighted_dataset_${d}_contrast_$c -d $PATH_OP/dataset_${d}_contrast_$c/ -tb $PATH_OP/tensorboard_d_${d}_c_${c} -r 0
+        python3.8 trainval.py -e weakly_spine_dataset_c6_red -sb $PATH_OP/results_dataset_${d}_contrast_$c -d $PATH_OP/dataset_${d}_contrast_$c/ -tb $PATH_OP/tensorboard_d_${d}_c_${c} -r 0
+        
+    done
+done
