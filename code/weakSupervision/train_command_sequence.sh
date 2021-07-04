cb71c58c1f446e311a19d7434afa3376334a7723
Jan Alexander <jan.alexander@verhaert.com>
2021-07-04 15:11:38 +0200

    adapt train command sequence script

--- code/weakSupervision/train_command_sequence.sh
+++ code/weakSupervision/train_command_sequence.sh
@@ -6,6 +6,16 @@ PATH_OP=/root/space/output
 for c in 3 
 
 do
+    for d in 2 1 0
+    do
+        
+        python3.8 trainval.py -e weakly_spine_dataset_c6_weighted -sb $PATH_OP/results_weighted_dataset_${d}_contrast_$c -d $PATH_OP/dataset_${d}_contrast_$c/ -tb $PATH_OP/tensorboard_d_${d}_c_${c} -r 0
+        
+    done
+done
+
+
+do
     for d in  1 0
     do
         
