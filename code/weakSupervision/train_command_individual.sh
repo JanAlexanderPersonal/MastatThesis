02c16be206bb5d6beb1a75d3537b7b0eb8598162
jalexander <jan.alexander@verhaert.com>
2021-08-06 13:09:44 +0200

    Start code for precalculated points

---
+++ code/weakSupervision/train_command_individual.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+PATH_DATA=/root/space/code/DataPreparation
+PATH_OP=/root/space/output
+c=3
+
+for d in 0 
+do
+    python3.8 trainval.py -e single_class -sb $PATH_OP/results_weighted_dataset_${d}_contrast_${c} -d $PATH_OP/dataset_${d}_contrast_$c/ -tb $PATH_OP/tensorboard_full_d_${d}_c_${c} -r 0
+    python3.8 trainval_Individual.py -e single_class -sb $PATH_OP/results_weighted_dataset_${d}_contrast_${c} -d $PATH_OP/dataset_${d}_contrast_$c/ -tb $PATH_OP/tensorboard_full_d_${d}_c_${c} -r 0
+done
+
+
+#for d in 1 
+#do
+    
+#    python3.8 trainval.py -e extra -sb $PATH_OP/results_weighted_dataset_${d}_contrast_${c} -d $PATH_OP/dataset_${d}_contrast_$c/ -tb $PATH_OP/tensorboard_full_d_${d}_c_${c} -r 0
+
+    
+#done
+
+
